5ec3684dbe91af82c7a9ef345e9546884ba3c8ac
Sven Lehnert <mail@sven-lehnert.de>
2017-04-07 18:14:56 +0200

    freemius integratin

--- loader.php
+++ loader.php
@@ -90,3 +90,83 @@ add_action('init', function(){
 		}
 	} );
 }, 1, 1);
+
+// Create a helper function for easy SDK access.
+function bfmod_fs() {
+	global $bfmod_fs;
+
+	if ( ! isset( $bfmod_fs ) ) {
+		// Include Freemius SDK.
+		if ( file_exists( dirname( dirname( __FILE__ ) ) . '/buddyforms/includes/resources/freemius/start.php' ) ) {
+			// Try to load SDK from parent plugin folder.
+			require_once dirname( dirname( __FILE__ ) ) . '/buddyforms/includes/resources/freemius/start.php';
+		} else if ( file_exists( dirname( dirname( __FILE__ ) ) . '/buddyforms-premium/includes/resources/freemius/start.php' ) ) {
+			// Try to load SDK from premium parent plugin folder.
+			require_once dirname( dirname( __FILE__ ) ) . '/buddyforms-premium/includes/resources/freemius/start.php';
+		} else {
+			require_once dirname(__FILE__) . 'includes/resources/freemius/start.php';
+		}
+
+		$bfmod_fs = fs_dynamic_init( array(
+			'id'                  => '409',
+			'slug'                => 'buddyforms-review',
+			'type'                => 'plugin',
+			'public_key'          => 'pk_b92e3b1876e342874bdc7f6e80d05',
+			'is_premium'          => false,
+			'has_paid_plans'      => false,
+			'parent'              => array(
+				'id'         => '391',
+				'slug'       => 'buddyforms',
+				'public_key' => 'pk_dea3d8c1c831caf06cfea10c7114c',
+				'name'       => 'BuddyForms',
+			),
+			'menu'                => array(
+				'slug'           => 'buddyforms',
+				'support'        => false,
+			),
+		) );
+	}
+
+	return $bfmod_fs;
+}
+
+function bfmod_fs_is_parent_active_and_loaded() {
+	// Check if the parent's init SDK method exists.
+	return function_exists( 'buddyforms_core_fs' );
+}
+
+function bfmod_fs_is_parent_active() {
+	$active_plugins_basenames = get_option( 'active_plugins' );
+
+	foreach ( $active_plugins_basenames as $plugin_basename ) {
+		if ( 0 === strpos( $plugin_basename, 'buddyforms/' ) ||
+		     0 === strpos( $plugin_basename, 'buddyforms-premium/' )
+		) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function bfmod_fs_init() {
+	if ( bfmod_fs_is_parent_active_and_loaded() ) {
+		// Init Freemius.
+		bfmod_fs();
+
+		// Parent is active, add your init code here.
+	} else {
+		// Parent is inactive, add your error handling here.
+	}
+}
+
+if ( bfmod_fs_is_parent_active_and_loaded() ) {
+	// If parent already included, init add-on.
+	bfmod_fs_init();
+} else if ( bfmod_fs_is_parent_active() ) {
+	// Init add-on only after the parent is loaded.
+	add_action( 'buddyforms_core_fs_loaded', 'bfmod_fs_init' );
+} else {
+	// Even though the parent is not activated, execute add-on for activation / uninstall hooks.
+	bfmod_fs_init();
+}
